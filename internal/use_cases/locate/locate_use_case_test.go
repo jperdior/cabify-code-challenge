258726f60df04c0c6fdc21ff856ab3160034f5ee
Julio Perdiguer <julio.perdiguer.extern@internetstores.com>
2024-01-14 00:08:26 +0100

    all tests validated

--- internal/use_cases/locate/locate_use_case_test.go
+++ internal/use_cases/locate/locate_use_case_test.go
@@ -11,45 +11,62 @@ import (
 func TestLocateUseCase_Locate(t *testing.T) {
 
 	locateUseCase := NewLocateUseCase()
-	carPool := carpool.NewCarPool()
 
 	t.Run("given a carpool with a group in a journey, it should return the car", func(t *testing.T) {
 		car1, err := carpool.NewCar(1, 4, 0)
+		require.NoError(t, err)
 		car2, err := carpool.NewCar(2, 5, 0)
+		require.NoError(t, err)
 		car3, err := carpool.NewCar(3, 5, 0)
 		require.NoError(t, err)
-		carPool.SetCars([]carpool.Car{car1, car2, car3})
-
+		cars := map[carpool.CarID]carpool.Car{
+			car1.ID(): car1, car2.ID(): car2, car3.ID(): car3,
+		}
+		carsByAvailableSeats := map[carpool.AvailableSeats]map[carpool.CarID]carpool.Car{
+			car1.AvailableSeats(): {car1.ID(): car1}, car2.AvailableSeats(): {car2.ID(): car2, car3.ID(): car3},
+		}
 		group, err := carpool.NewGroup(1, 2)
 		require.NoError(t, err)
-		err = carPool.AddGroup(group)
+		groups := map[carpool.GroupID]carpool.Group{group.ID(): group}
+		journey, err := carpool.NewJourney(group, car2)
 		require.NoError(t, err)
+		journeys := map[carpool.GroupID]carpool.Journey{group.ID(): journey}
+		carPool := carpool.NewTestCarPoolWithCarsAndJourneys(
+			cars,
+			carsByAvailableSeats,
+			groups,
+			journeys,
+		)
 
-		err = carPool.Journey(group)
-		require.NoError(t, err)
-		err = carPool.Journey(group)
-		require.NoError(t, err)
-
-		locationCar, err := locateUseCase.Locate(carPool, 1)
-		expectedCar, err := carpool.NewCar(1, 4, 2)
+		locationCar, err := locateUseCase.Locate(carPool, group.ID().Value())
+		expectedCar, err := carpool.NewCar(car2.ID().Value(), car2.Seats().Value(), car2.AvailableSeats().Value())
 
 		require.NoError(t, err)
+		// should be in car2
 		assert.Equal(t, expectedCar, locationCar)
 	})
 
 	t.Run("given a carpool with a group waiting, it should return an empty car", func(t *testing.T) {
 		group, err := carpool.NewGroup(1, 2)
 		require.NoError(t, err)
-		carPool.AddGroup(group)
-		carPool.AddWaitingGroup(group)
+		groups := map[carpool.GroupID]carpool.Group{group.ID(): group}
+		waitingGroups := []carpool.Group{group}
+		waitingGroupsIndexHash := map[carpool.GroupID]int{group.ID(): 0}
 
-		locationCar, err := locateUseCase.Locate(carPool, 1)
+		carPool := carpool.NewTestCarPoolWithWaitingGroups(
+			groups,
+			waitingGroups,
+			waitingGroupsIndexHash,
+		)
+
+		locationCar, err := locateUseCase.Locate(carPool, group.ID().Value())
 		require.NoError(t, err)
 
 		assert.Equal(t, carpool.Car{}, locationCar)
 	})
 
 	t.Run("given a carpool without the group we want to locate it should return an error", func(t *testing.T) {
+		carPool := carpool.NewCarPool()
 		_, err := locateUseCase.Locate(carPool, 1)
 		assert.True(t, errors.Is(err, carpool.ErrGroupNotFound))
 	})
